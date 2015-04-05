698cdd53c30b95946891af175131f144e1987c87
unknown <brownblair@apphabor.com>
2015-04-05 11:15:42 -0700

    initial Commit

---
+++ AutoEdu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoEdu.Startup))]
+namespace AutoEdu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
